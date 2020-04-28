2d50b0a83b6fbc38555a5eef4d98adcdb4ea3563
Anoop Singh <anoopcbjnu@gmail.com>
2020-04-28 16:14:24 +0100

    This is about for loop

---
+++ for_loop1.py
@@ -0,0 +1,31 @@
+## to print("hello world") 10 times by for loop
+
+for i in range(10):
+
+    print(f"hello world :{i}")
+
+
+for i in range(20):
+
+    print(f"hello world :{i}")
+
+
+## sum for a range from 1 to 50 by for loop
+
+total = 0
+
+for num in range(1, 51):
+
+    total += num
+print(total)
+
+
+n = int(input("Please enter a number "))
+
+
+total = 0
+
+for num in range(1, n+1):
+
+    total += num
+print(total)
